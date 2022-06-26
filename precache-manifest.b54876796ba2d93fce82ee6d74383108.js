3c3645190aadfdbf6de3e8f526f80b482b17a5fe
MohsinBaig101 <mbaigarid@gmail.com>
2022-06-26 14:48:02 +0500

    build create

---
+++ precache-manifest.b54876796ba2d93fce82ee6d74383108.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e49cbf8196d5ad43f07aca967cde74da",
+    "url": "./index.html"
+  },
+  {
+    "revision": "eab277152abc8a78b8d7",
+    "url": "./static/css/2.d39e68e1.chunk.css"
+  },
+  {
+    "revision": "a86333f31a3eca6045c4",
+    "url": "./static/css/main.e8901382.chunk.css"
+  },
+  {
+    "revision": "eab277152abc8a78b8d7",
+    "url": "./static/js/2.86903406.chunk.js"
+  },
+  {
+    "revision": "0297ac78c823ef9b172e38637e06e92e",
+    "url": "./static/js/2.86903406.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a86333f31a3eca6045c4",
+    "url": "./static/js/main.ef2d76b2.chunk.js"
+  },
+  {
+    "revision": "61e5e8c107e98626f2ce",
+    "url": "./static/js/runtime-main.3a088ac7.js"
+  }
+]);
